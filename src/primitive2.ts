e25e4b2263c8a7be287f4d777a56ed1c73f21ea1
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2023-08-02 22:53:42 +0900

    2.4.6の変更

--- src/primitive2.ts
+++ src/primitive2.ts
@@ -7,7 +7,7 @@ const rl = createInterface({
 
 rl.question('名前を入力してください:', (name) => {
     // 文字列が入力されるとココが実行される
-    // const num = Number(line);
-    console.log("こんにちは、" + name + "さん");
+    const displayName = name || "名無し";
+    console.log(`こんにちは、${displayName}さん`);
     rl.close;
 });
